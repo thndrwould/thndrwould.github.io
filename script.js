c1644981c5edaf41103bd735415a40eeb2c9dd85
thndrwould <thndrwould@gmail.com>
2026-07-02 01:58:58 -0400

    v0.11 actually
    uh bruh

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const button = document.getElementById('actionBtn');
 const resButton = document.getElementById('resetBtn');
+const roblox = document.getElementById('roblox');
+const back = document.getElementById('backG')
 const greeting = document.getElementById('greeting');
+
 
 // Add a click event to the button
 button.addEventListener('click', () => {
-    greeting.textContent = "Steam Frame";
+    greeting.textContent = "CLICKED BRUH";
     greeting.style.color = "#ff0000"; // Changes text to green
     greeting.style.outline = true;
     greeting.style.outlineColor = "#000000";
@@ -15,3 +18,10 @@ resButton.addEventListener('click', () => {
     greeting.style.color = "#000000"; // Changes text to black
 });
 
+roblox.addEventListener('mouseenter', () => {
+    back.style.opacity = 1;
+});
+
+roblox.addEventListener('mouseleave', () => {
+    back.style.opacity = 0;
+});
